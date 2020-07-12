61329228f55f2b9c6de5f455bfe09a5b3ffe6035
janeriklee <64036526+janeriklee@users.noreply.github.com>
2020-07-12 22:23:12 +0300

    Update esx_drugs.sql

--- esx_drugs.sql
+++ esx_drugs.sql
@@ -1,6 +1,6 @@
 USE `es_extended`;
 
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
 	('cannabis', 'Cannabis', 3, 0, 1),
 	('marijuana', 'Marijuana', 2, 0, 1)
 ;
